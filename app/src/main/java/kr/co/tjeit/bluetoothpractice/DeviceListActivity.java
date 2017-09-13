020e63903ec0bd85c2c7f8e8fb7c5de1405acee2
hyuni106 <30651511+hyuni106@users.noreply.github.com>
2017-09-13 09:46:47 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjeit/bluetoothpractice/DeviceListActivity.java
@@ -0,0 +1,31 @@
+package kr.co.tjeit.bluetoothpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeviceListActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_list);
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
